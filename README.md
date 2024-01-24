718ec442ae7b6a3cf682828039177fb4ee3f1046
abedin-ashraf <nurulabedinashraf@gmail.com>
2024-01-23 20:06:31 -0600

    Added sidebar component

---
+++ README.md
@@ -0,0 +1,4 @@
+Material UI -> For Icon
+FireBase -> Realtime Database
+React-Flip-Move -> Animation
+Deploy using Firebase
---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    //BEM Convention
+    <div className="app">
+      <h1>Ashraf</h1>
+
+      {/*Sidebar  */}
+      <Sidebar />
+
+
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
